fef03df73f7568b3dcb8cf281d26a02790dcdf3a
Esteban Russo <oerusso28@gmail.com>
2023-09-07 23:04:36 -0300

    tipos de metodos http

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('Hello world!')
+    res.send('Hello world!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('the current weather is nice')
+})
+
+app.use((req, res) => {
+    res.status(404).send('not found your page')
 })
 
 app.listen(3000);
